34d2f4ea4170fbf5a1fdec407cc9040dc79a5489
hukunge <66547109@qq.com>
2017-07-12 13:09:10 +0800

    a

--- RxJavaSamples-master/app/src/main/java/com/rengwuxian/rxjavasamples/model/FakeThing.java
+++
@@ -1,8 +0,0 @@
-// (c)2016 Flipboard Inc, All Rights Reserved.
-
-package com.rengwuxian.rxjavasamples.model;
-
-public class FakeThing {
-    public int id;
-    public String name;
-}
--- RxJavaSamples-master/app/src/main/java/com/rengwuxian/rxjavasamples/model/Item.java
+++
@@ -1,8 +0,0 @@
-// (c)2016 Flipboard Inc, All Rights Reserved.
-
-package com.rengwuxian.rxjavasamples.model;
-
-public class Item {
-    public String description;
-    public String imageUrl;
-}
